07fd48fd7cb1b83d0bade7bfd72c63e222258dd7
Jim <jimochieng64@gmail.com>
2026-03-27 02:27:17 +0300

    Connected to database and read values

--- connecting-to-db.py
+++ connecting-to-db.py
@@ -1,2 +1,16 @@
 import sqlite3
 import pandas as pd
+
+conn = sqlite3.connect('data.sqlite')
+
+cur = conn.cursor()
+
+df = pd.DataFrame(
+    cur.execute("""SELECT * FROM offices;""").fetchall(),
+
+    columns=[x[0] for x in cur.description]
+)
+
+print(df)
+
+conn.close()
